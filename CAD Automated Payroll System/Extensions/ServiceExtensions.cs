03f0f412e3bdd93e508ce9bddac7c2db89b275a6
Emmanuel Nzekwe <emmanuelnzekwe24@gmail.com>
2021-03-12 23:50:20 +0100

    feat: initial commit

---
+++ CAD Automated Payroll System/Extensions/ServiceExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CAD_Automated_Payroll_System.Extensions
+{
+    public class ServiceExtensions
+    {
+    }
+}
